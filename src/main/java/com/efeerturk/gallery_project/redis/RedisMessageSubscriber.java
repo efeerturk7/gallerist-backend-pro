f7d84c770df09c7ef099a04f4a2ebb26c67c4a63
bahadirefeerturk <bahadirefeerturk@gmail.com>
2026-02-13 22:24:02 +0300

    fix bean

--- src/main/java/com/efeerturk/gallery_project/redis/RedisMessageSubscriber.java
+++ src/main/java/com/efeerturk/gallery_project/redis/RedisMessageSubscriber.java
@@ -3,7 +3,9 @@ package com.efeerturk.gallery_project.redis;
 import org.jspecify.annotations.Nullable;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RedisMessageSubscriber implements MessageListener {
     @Override
     public void onMessage(Message message, byte @Nullable [] pattern) {
